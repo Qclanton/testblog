705dff6d7d72276c49839a64b968595f1b1d9782
Anton Qcl <qcl.anton@gmail.com>
2015-09-08 16:45:24 -0400

    Added transactions in Mysql driver
    Created page /test for tests.
    Modified Mysql driver - fixed bugs and added transactions support

--- app/Libs/Qdata/Mysql.php
+++ app/Libs/Qdata/Mysql.php
@@ -2,24 +2,28 @@
 namespace Testblog\Libs\Qdata;
 
 class Mysql {
-    private $connection;    
+    private $connection;
+    private $is_transaction_started = false;
+    private $is_transaction_failed = false;    
     
-    public function __construct($host="localhost", $user="root", $password="", $schema="", $port=3306) {
+    
+    
+    
+
+    public function __construct($host="localhost", $user="root", $password="", $schema="", $port=3306) 
+    {
         $connection = new \mysqli($host, $user, $password, $schema, $port);
 
         if ($connection->connect_error) {
             throw new \Exception("Connection failed: {$connection->connect_error}");
-        }
-        
+        }        
         
         $this->connection = $connection;
-        $this->execute(" SET NAMES 'UTF8' ");
-        $result = true;        
-        
-        return $result;    
+        $this->execute(" SET NAMES 'UTF8' ");      
     }
     
-    public function closeConnection($connection = null) {    
+    public function closeConnection($connection = null) 
+    {    
         $connection->close;
         
         return $this;
@@ -28,70 +32,64 @@ class Mysql {
     
     
     
-    private function getReferences($array) {      
-        foreach($array as $key => $value) {
-            $references[$key] = &$array[$key];
-        }
-        
-        return $references; 
-    }
     
-    private function prepareStatement($query, array $params = []) {
-        $statement = $this->connection->prepare($query);        
-
+    private function prepareStatement($query, array $params = [])
+    {
+        // Prepare query
+        $statement = $this->connection->prepare($query);     
         if (!$statement) {
-            throw new \Exception("Preparing failed: " . $this->connection->error);
+            throw new \Exception("Preparing failed. Error: {$this->connection->error}");
         }
         
+        
+        // Bind params
         if (!empty($params)) {
-            $params_types = "";
-            $params_values = [];
-            foreach ($params as $param) {
-                $param_type = (is_array($param) ? current($param) : "s");
-                $param_value = (is_array($param) ? key($var) : $param);
-                
-                $params_types .= $param_type;
-                $params_values[] = $param_value;
-            }
-            array_unshift($params_values, $params_types);
-            call_user_func_array([$statement, "bind_param"], $this->getReferences($params_values)); 
-        }        
+			$types = "";            
+			foreach ($params as $index=>$param) {
+				${$index} = (is_array($param) ? key($param) : $param);
+											   
+				$types .= (is_array($param) ? current($param) : "s");
+				$values[] = &${$index};
+			}
+			array_unshift($values, $types);		
+			
+			
+			call_user_func_array([$statement, "bind_param"], $values); 
+		}    
         
         
         return $statement;        
     }
-    
-    
-    
-    
-    
-    public function getRows($query, array $params = []) {
-        $statement = $this->prepareStatement($query, $params);
-        if (!$statement) { 
-            throw new \Exception ("Preaparing of statement failed. Error: " . $this->connection->error());
-        }
-        
+
+
+
+
+
+    public function getRows($query, array $params = []) 
+    {
+        $statement = $this->prepareStatement($query, $params);        
         $statement->execute();        
+           
            
         $meta = $statement->result_metadata();
         while ($field = $meta->fetch_field()) { 
-             $columns[] = &$row[$field->name]; 
+            $columns[] = &$row[$field->name]; 
         } 
-
-        call_user_func_array([$statement, "bind_result"], $columns); 
-
-        $result = null;
-        while ($statement->fetch()) {            
-            foreach ($row as $key=>$value) {
-                $row_data[$key] = $value;
-            }
-                     
-            $result[] = (object)$row_data;
-        }
-                
-        $statement->close();        
+        call_user_func_array([$statement, "bind_result"], $columns);   
         
-        return $result;    
+        
+        $result = null;
+        while ($statement->fetch()) {
+			foreach ($row as $key=>$value) {
+				$row_data[$key] = $value;
+			}
+                   
+            $result[] = (object)$row_data;
+        }           
+        $statement->close(); 
+        
+        
+        return $result;
     }
         
     public function getRow($query, array $params = []) {
@@ -110,12 +108,57 @@ class Mysql {
     }
     
 
+
     public function execute($query, array $params = []) {
-        $statement = $this->prepareStatement($query, $params);
-        $statement->execute();
-        $statement->close();
+		// Handle errors during preparing statements
+		try {
+			$statement = $this->prepareStatement($query, $params);
+			$statement->execute();                    
+        } catch (\Exception $e) {
+            if ($this->is_transaction_started) {
+                $this->is_transaction_failed = true;  
+                $this->transactionEnd();
+            }
+            
+            throw $e;
+        }
         
-        return true;    
+		// Handle errors during execituin query
+        if ($statement->error) {
+			if ($this->is_transaction_started) {
+                $this->is_transaction_failed = true;  
+                $this->transactionEnd();
+			}
+			
+			throw new \Exception("Query execution error: {$statement->error}");			
+		}
+		
+
+		$statement->close();
+        
+        
+        return $this;    
+    }
+    
+    public function transactionStart() {
+        $this->is_transaction_started = true;
+        $this->is_transaction_failed = false;
+        $this->connection->begin_transaction();
+        
+        return $this;
+    }
+    
+    public function transactionEnd() {
+		if ($this->is_transaction_started) {
+			!$this->is_transaction_failed
+				? $this->connection->commit()
+				: $this->connection->rollback()
+			;
+		}
+        
+        $this->is_transaction_started = false;
+        
+        return $this;
     }
 }
 ?>
--- app/index.php
+++ app/index.php
@@ -5,15 +5,57 @@ require __DIR__ . "/../vendor/autoload.php";
 $app = new \Slim\Slim(['debug'=>true]);
 $app->view->setTemplatesDirectory(__DIR__ . "/views");
 
-$Data = new Testblog\Libs\Qdata\Qdata("localhost", "root", "111222", "testblog");
-// echo "<pre>"; var_dump($Data); echo "<pre>";
-
 // Just render template
 $app->get('/(:layout)(/)', function ($layout="feed") use ($app) {
-    in_array($layout, ["feed", "add"])
-        ? $app->render($layout . ".php")
-        : $app->notFound()
-    ;
+    if ($layout === "test") {
+        $Database = new Testblog\Libs\Qdata\Mysql("localhost", "root", "111222", "testblog");
+        // echo "<pre>"; var_dump($Database); echo "<pre>";
+        
+        
+        $Database
+            ->transactionStart()
+                ->execute("UPDATE `posts` SET `text`=? WHERE `id`=?", ["YYYYYYYYYYYYY", 1])
+                ->execute("UPDATE `posts` SET `text`=? WHERE `id`=?", ["XXXXXXXXXX", 2])
+                ->execute("INSERT INTO `posts` (`title`, `text`) VALUES (?, ?)", ["test", "test"])
+            ->transactionEnd()
+        ;
+        
+        // $Database->execute("INSERT INTO `posts`  VALUES ('1', 'test', 'test')");
+        
+        // $Database->execute("UPDATE `posts` SET `text`=? WHERE `id`=?", ["SSSSSSSSSSS", 1]);
+        // $Database->execute("UPDATE `posts` SET `text`=? WHERE `id`=?", ["KKKKK", 1]);
+        
+        /*
+        $posts = $Database->getRows("SELECT * FROM `posts` WHERE `id` IN (?,?)", [1,2]);
+        echo "---POSTS---" . nl2br(PHP_EOL);
+        foreach ($posts as $post) {            
+            echo "<pre>"; var_dump($post); echo "<pre>";
+        }
+        */
+        /*
+        $Database
+            ->transactionStart()
+                ->execute()
+        */
+        
+        /*
+        $post = $Database->getValue("SELECT `title` FROM `posts` WHERE `id` = ?", [1]);
+        echo "<pre>"; var_dump($post); echo "<pre>";
+        */
+        
+        /*
+        $posts = $Database->getRows("SELECT * FROM `posts` WHERE `id` IN (?,?)", [1,2]);
+        echo "---POSTS---" . nl2br(PHP_EOL);
+        foreach ($posts as $post) {            
+            echo "<pre>"; var_dump($post); echo "<pre>";
+        }
+        */
+    } else {
+        in_array($layout, ["feed", "add"])
+            ? $app->render($layout . ".php")
+            : $app->notFound()
+        ;
+    }
 });
 
 $app->run();
